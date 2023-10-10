00ae2821fc61b0483e64c74158f28211c3ef2864
alex-mngt <mongeot.alex@gmail.com>
2023-10-11 00:54:27 +0200

    chore: prevent indexing during dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Averse",
   description: "Averse is a jewelery brand.",
+  robots: "noindex",
 };
 
 const RootLayout: FC<PropsWithChildren> = (props) => {
