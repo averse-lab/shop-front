fe0c71d0bc0072df1940b0ee150d37d8f9045e41
alex-mngt <mongeot.alex@gmail.com>
2023-10-24 17:43:00 +0200

    fix : prefetch shop section

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -8,6 +8,8 @@ import { Locale } from "@lib/i18n/types";
 
 import { HOME_VIDEO, SECTIONS } from "./_internal/HomePage.constants";
 import { SectionsKey } from "./_internal/HomePage.types";
+import { CATEGORIES } from "./shop/_internal/ShopPage.constants";
+import { CategoriesKey } from "./shop/_internal/ShopPage.types";
 
 export async function generateStaticParams() {
   return I18N_CONFIG.locales.map((locale) => ({ lang: locale }));
@@ -22,6 +24,7 @@ const HomePage: FC<IProps> = async (props) => {
   const { lang } = params;
 
   const shopSection = SECTIONS.get(SectionsKey.SHOP)!;
+  const allProductsCategory = CATEGORIES.get(CategoriesKey.ALL_PRODUCTS)!;
 
   return (
     <div className='relative h-screen flex flex-col justify-center items-center text-center p-4'>
@@ -33,7 +36,10 @@ const HomePage: FC<IProps> = async (props) => {
       />
       <h1 className='text-4xl font-bold text-white  mb-2'>Averse</h1>
       <p className='mb-2'>HAND ENGRAVER AND JEWELLER BASED IN PARIS</p>
-      <Button element='link' href={`/${lang}/${shopSection.url}`}>
+      <Button
+        element='link'
+        href={`/${lang}/${shopSection.url}/${allProductsCategory.url}`}
+      >
         Go to shop
       </Button>
     </div>
