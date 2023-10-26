6b64e3a41d438fec2eb28a0085d73d5e0a132bd9
alex-mngt <mongeot.alex@gmail.com>
2023-10-26 02:23:28 +0200

    chore: Menu UI improvements

--- src/components/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -2,7 +2,11 @@
 
 import { FC, useRef, useState } from "react";
 
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowUpRightIcon,
+  Bars3Icon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 import { clsx } from "clsx";
 import Link from "next/link";
 
@@ -25,45 +29,75 @@ export const BurgerMenu: FC<IProps> = (props) => {
   const [open, setOpen] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
 
-  const handleBurgerClick = () => {
+  const openMenu = () => {
     setOpen(true);
   };
 
-  const handleCloseClick = () => {
+  const closeMenu = () => {
     setOpen(false);
   };
 
   useBodyScrollLocker(open);
-  useClickOutsideDetector(menuRef.current, handleCloseClick, open === true);
+  useClickOutsideDetector(menuRef.current, closeMenu, open === true);
 
   return (
     <>
-      <button className={clsx(s["burger-menu__burger"], className)}>
+      <button className={clsx(className, s["burger-menu__burger"])}>
         <Bars3Icon
-          className={`w-6 h-6 lg:hover:stroke-[1.75] lg:hover:scale-105 transition-all`}
-          onClick={handleBurgerClick}
+          className={clsx(
+            "w-6 h-6",
+            "lg:hover:stroke-[1.75] lg:hover:scale-105 transition-all",
+          )}
+          onClick={openMenu}
         />
       </button>
       <div
         ref={menuRef}
-        className={`${s["burger-menu__menu"]} ${
-          open ? s["burger-menu__menu--open"] : null
-        } fixed flex flex-col  z-20 h-full md:h-auto w-full md:w-auto p-6 uppercase bg-black top-0 left-0 md:top-2 md:left-2 md:rounded md:shadow-md`}
+        className={clsx(
+          s["burger-menu__menu"],
+          open && s["burger-menu__menu--open"],
+          "fixed z-20 top-0 left-0 md:top-2 md:left-2",
+          "flex flex-col",
+          "h-full md:h-auto w-full md:w-auto p-6",
+          "uppercase bg-black  md:rounded md:shadow-md",
+        )}
       >
         <XMarkIcon
-          onClick={handleCloseClick}
-          className={`w-6 h-6 cursor-pointer text-white self-end lg:hover:stroke-2 lg:hover:scale-105 transition-all`}
+          onClick={closeMenu}
+          className={clsx(
+            "self-end",
+            "w-6 h-6",
+            "cursor-pointer text-white lg:hover:stroke-2 lg:hover:scale-105 transition-all",
+          )}
         />
-        <div className='flex flex-1 flex-col items-center justify-center gap-6 md:gap-4 md:px-24 md:py-16'>
+        <div
+          className={clsx(
+            "flex flex-1 flex-col items-center justify-center gap-6 md:gap-4",
+            "md:px-24 md:py-16",
+          )}
+        >
           {nav.map((navItem) => (
-            <Link
-              className={`${s["burger-menu__link"]} text-xl md:text-base text-white relative`}
+            <div
+              className={clsx(s["burger-menu__link"], "relative", "text-white")}
               key={navItem.display}
-              href={`/${lang}/${navItem.url}`}
-              onClick={handleCloseClick}
             >
-              {navItem.display}
-            </Link>
+              <Link
+                className={clsx("relative", "text-xl md:text-base")}
+                href={`/${lang}/${navItem.url}`}
+                onClick={closeMenu}
+              >
+                {navItem.display}
+              </Link>
+              <ArrowUpRightIcon
+                className={clsx(
+                  s["burger-menu__link-icon"],
+                  "hidden lg:block",
+                  "absolute top-0 bottom-0 m-auto",
+                  "w-4 h-4",
+                  "stroke-2",
+                )}
+              />
+            </div>
           ))}
         </div>
       </div>
