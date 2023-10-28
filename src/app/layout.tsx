73c47c215f6d8fb40ab3d4277037182b312afa2f
Thibault Walterspieler <twalterspieler@pm.me>
2023-10-28 22:34:01 +0200

    feat: add favicon & manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,29 @@ import "@averse/app/globals.css";
 
 import { FC, PropsWithChildren } from "react";
 
+import Head from "next/head";
+
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Averse",
-  description: "Averse is a jewelery brand.",
+  description:
+    "Averse is a jewelry atelier. Products are imagined in Paris, and made in France, using precious metals, and gems.",
   robots: "noindex",
 };
 
 const RootLayout: FC<PropsWithChildren> = (props) => {
   const { children } = props;
 
-  return children;
+  return (
+    <>
+      <Head>
+        <link rel='icon' href='/favicon.ico' sizes='any' />
+        <meta name='theme-color' content='#000000' />
+      </Head>
+      {children}
+    </>
+  );
 };
 
 export default RootLayout;
