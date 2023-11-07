6b42cb9f3abd9b3c8e1cea9da51fa8811dabf43d
Thibault Walterspieler <twalterspieler@pm.me>
2023-11-07 12:48:19 +0100

    feat: temporary change the playbackId of category

--- src/app/[lang]/shop/[category]/_internal/CategoryPage.constants.ts
+++ src/app/[lang]/shop/[category]/_internal/CategoryPage.constants.ts
@@ -5,37 +5,37 @@ import { Animation } from "./CategoryPage.types";
 export const ANIMATIONS: Record<CategoryKey, Animation[]> = {
   allProducts: [
     {
-      playbackId: "Su26DLxRkc1MJJ5D1wBtqtcImwsXcaCw9855EGlu3k4",
+      playbackId: "ZlPchSa00ChslK6J3YHtbujKipseamwByC802grWizBxc",
       gridIndex: 2,
       gridDesktopIndex: 2,
     },
     {
-      playbackId: "Su26DLxRkc1MJJ5D1wBtqtcImwsXcaCw9855EGlu3k4",
+      playbackId: "ZlPchSa00ChslK6J3YHtbujKipseamwByC802grWizBxc",
       gridIndex: 7,
       gridDesktopIndex: 4,
     },
     {
-      playbackId: "Su26DLxRkc1MJJ5D1wBtqtcImwsXcaCw9855EGlu3k4",
+      playbackId: "ZlPchSa00ChslK6J3YHtbujKipseamwByC802grWizBxc",
       gridIndex: 10,
       gridDesktopIndex: 11,
     },
     {
-      playbackId: "Su26DLxRkc1MJJ5D1wBtqtcImwsXcaCw9855EGlu3k4",
+      playbackId: "ZlPchSa00ChslK6J3YHtbujKipseamwByC802grWizBxc",
       gridIndex: 15,
       gridDesktopIndex: 13,
     },
     {
-      playbackId: "Su26DLxRkc1MJJ5D1wBtqtcImwsXcaCw9855EGlu3k4",
+      playbackId: "ZlPchSa00ChslK6J3YHtbujKipseamwByC802grWizBxc",
       gridIndex: 18,
       gridDesktopIndex: 19,
     },
     {
-      playbackId: "Su26DLxRkc1MJJ5D1wBtqtcImwsXcaCw9855EGlu3k4",
+      playbackId: "ZlPchSa00ChslK6J3YHtbujKipseamwByC802grWizBxc",
       gridIndex: 23,
       gridDesktopIndex: 20,
     },
     {
-      playbackId: "Su26DLxRkc1MJJ5D1wBtqtcImwsXcaCw9855EGlu3k4",
+      playbackId: "ZlPchSa00ChslK6J3YHtbujKipseamwByC802grWizBxc",
       gridIndex: 26,
       gridDesktopIndex: 26,
     },
