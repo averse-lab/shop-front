aaae38aafaa0def4e9c06ad15ce4b82cfc6c64af
alex-mngt <mongeot.alex@gmail.com>
2023-11-15 17:25:15 +0100

    chore: use I18N_LOCALES instead of local copies

--- src/lib/i18n/config.ts
+++ src/lib/i18n/config.ts
@@ -1,6 +1,8 @@
-import { Config } from "./types";
+import { Config, Locale } from "./types";
+
+export const I18N_LOCALES: Locale[] = ["en", "fr"];
 
 export const I18N_CONFIG: Config = {
   defaultLocale: "en",
-  locales: ["en", "fr"],
+  locales: I18N_LOCALES,
 };
