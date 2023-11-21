be779960d859f6a57a6728f0b039fb3b0a7b8d36
alex-mngt <mongeot.alex@gmail.com>
2023-11-21 23:21:21 +0100

    feat: create BaseLayout component

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,17 +1,9 @@
 import { FC, PropsWithChildren } from "react";
 
-import { clsx } from "clsx";
+import { BaseLayout } from "@components/BaseLayout/BaseLayout";
 
-import { Footer } from "@components/Footer/Footer";
-import { Header } from "@components/Header/Header";
-
-import { DMSans } from "@lib/fonts";
 import { Locale } from "@lib/i18n/types";
 import { getDictionary } from "@lib/i18n/utils";
-import { combineProviders } from "@lib/utils";
-
-import { CartContextProvider } from "@contexts/CartContext/CartContext";
-import { HeaderContextProvider } from "@contexts/HeaderContext/HeaderContext";
 
 type IProps = {
   params: { lang: Locale };
@@ -23,32 +15,10 @@ const LangLayout: FC<IProps> = async (props) => {
 
   const dictionary = await getDictionary(lang);
 
-  const AppProvider = combineProviders([
-    CartContextProvider,
-    HeaderContextProvider,
-  ]);
-
   return (
-    <html lang={lang}>
-      <body
-        className={clsx(
-          DMSans.variable,
-          "mb-[220px] lg:mb-[204px]",
-          "min-h-screen",
-          "flex flex-col",
-          "bg-black shadow-md",
-          "font-sans",
-        )}
-      >
-        <AppProvider>
-          <Header dictionary={dictionary} lang={lang} />
-          <main className={clsx("flex grow flex-col", "bg-white")}>
-            {children}
-          </main>
-          <Footer dictionary={dictionary} lang={lang} />
-        </AppProvider>
-      </body>
-    </html>
+    <BaseLayout dictionary={dictionary} lang={lang}>
+      {children}
+    </BaseLayout>
   );
 };
 
