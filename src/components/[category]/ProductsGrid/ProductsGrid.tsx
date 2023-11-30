f144411359120a333cbb26220934922bda60ca99
alex-mngt <mongeot.alex@gmail.com>
2023-11-30 02:06:22 +0100

    chore: smoothing animation

--- src/components/[category]/ProductsGrid/ProductsGrid.tsx
+++ src/components/[category]/ProductsGrid/ProductsGrid.tsx
@@ -86,11 +86,11 @@ export const ProductsGrid: FC<IProps> = (props) => {
               <div ref={ref}></div>
               <FilterSelector
                 className={clsx(
-                  "fixed bottom-5 left-0 right-0 z-10 m-auto",
+                  "fixed bottom-2 left-0 right-0 z-10 m-auto",
                   "transition-all duration-200 ease-out",
                   inView
-                    ? "translate-y-1 opacity-0"
-                    : "-translate-y-1 opacity-100",
+                    ? "translate-y-3 opacity-0"
+                    : "-translate-y-3 opacity-100",
                 )}
                 filters={filters}
                 lang={lang}
