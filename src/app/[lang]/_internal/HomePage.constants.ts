21e26979fb604772e49dc460bfbc6fa03fb53909
Thibault Walterspieler <twalterspieler@pm.me>
2024-05-10 18:32:53 +0200

    feat: change home video

--- src/app/[lang]/_internal/HomePage.constants.ts
+++ src/app/[lang]/_internal/HomePage.constants.ts
@@ -1,7 +1,7 @@
 import { MuxVideoSource } from "@lib/types";
 
 export const HOME_VIDEO: MuxVideoSource = {
-  playbackId: "NyIWzslBMOwfsQBPm9e1rrdksCf4g6YnDeV9uvN8Ryo",
+  playbackId: "5bEniVqAzmx6N8lDr4wxF5iaLH02Wo7uxP4txpl9Hn5U",
   widthRatio: 16,
   heightRatio: 9,
 };
