6576e77d13847ab8b51e44e42492dccf044ef536
Thibault Walterspieler <twalterspieler@pm.me>
2024-05-10 18:35:25 +0200

    fix: use public mux id

--- src/app/[lang]/_internal/HomePage.constants.ts
+++ src/app/[lang]/_internal/HomePage.constants.ts
@@ -1,7 +1,7 @@
 import { MuxVideoSource } from "@lib/types";
 
 export const HOME_VIDEO: MuxVideoSource = {
-  playbackId: "5bEniVqAzmx6N8lDr4wxF5iaLH02Wo7uxP4txpl9Hn5U",
+  playbackId: "wFPi9zVWnsbQgv8zaTSOZ01A5ezvfKt2U2a026h602KVwI",
   widthRatio: 16,
   heightRatio: 9,
 };
