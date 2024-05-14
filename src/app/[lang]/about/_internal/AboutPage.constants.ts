92b7fea344594cfd6a5b3e3c884424e0b6b08b81
Thibault Walterspieler <twalterspieler@pm.me>
2024-05-14 18:05:06 +0200

    feat: add content to about page

--- src/app/[lang]/about/_internal/AboutPage.constants.ts
+++ src/app/[lang]/about/_internal/AboutPage.constants.ts
@@ -1,7 +1,21 @@
 import { MuxVideoSource } from "@lib/types";
 
-export const ABOUT_VIDEO: MuxVideoSource = {
-  playbackId: "DX8006bB01OzhuylmJD1hzTOtKCXOe7fdwToVTDDuT3EI",
-  heightRatio: 1,
-  widthRatio: 1,
+const ABOUT_VIDEO: MuxVideoSource = {
+  playbackId: "jhY9018lo4fttLUzK9bvV3n9sPG02bmapl9ETCWkFYrdE",
+  widthRatio: 4,
+  heightRatio: 3,
 };
+
+const IRRATIONAL_CUBE_VIDEO: MuxVideoSource = {
+  playbackId: "VnSqYFPMv7ckRpbLL1D015q6FtO3L1LGJZB6dDQEDQB00",
+  widthRatio: 4,
+  heightRatio: 5,
+};
+
+const CRYING_GIRL_VIDEO: MuxVideoSource = {
+  playbackId: "E02k4Wn01ZgNuj3oIdmYok3k9M02n4rir02duqyDrIKodoY",
+  widthRatio: 347,
+  heightRatio: 450,
+};
+
+export { ABOUT_VIDEO, CRYING_GIRL_VIDEO, IRRATIONAL_CUBE_VIDEO };
--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -11,7 +11,11 @@ import { getDictionary } from "@lib/i18n/utils";
 import { PAGES } from "@lib/routing/constants";
 import { generateAlternates } from "@lib/utils";
 
-import { ABOUT_VIDEO } from "./_internal/AboutPage.constants";
+import {
+  ABOUT_VIDEO,
+  CRYING_GIRL_VIDEO,
+  IRRATIONAL_CUBE_VIDEO,
+} from "./_internal/AboutPage.constants";
 
 export async function generateMetadata(props: IProps): Promise<Metadata> {
   const { params } = props;
@@ -64,8 +68,6 @@ const AboutPage: FC<IProps> = async (props) => {
   const { params } = props;
   const { lang } = params;
 
-  const { playbackId, heightRatio, widthRatio } = ABOUT_VIDEO;
-
   const dictionary = await getDictionary(lang);
 
   return (
@@ -79,25 +81,34 @@ const AboutPage: FC<IProps> = async (props) => {
             "m-auto mt-[72px] md:mt-[96px]",
             "lg:max-w-[550px]",
             "flex flex-1 flex-col items-center justify-center",
-            "text-center text-white",
+            "text-left text-white",
           )}
         >
           <VideoPlayer
-            className={clsx("mb-12", "max-w-[80%] md:max-w-[250px]")}
-            heightRatio={heightRatio}
-            playbackId={playbackId}
-            widthRatio={widthRatio}
+            className={clsx("mb-12", "max-w-[90%] md:max-w-[400px]")}
+            heightRatio={ABOUT_VIDEO.heightRatio}
+            playbackId={ABOUT_VIDEO.playbackId}
+            widthRatio={ABOUT_VIDEO.widthRatio}
           />
-          <p>{dictionary.about.paragraph1}</p>
-          <p className={clsx("mb-4")}>{dictionary.about.paragraph2}</p>
-          <p className={clsx("mb-4")}>{dictionary.about.paragraph3}</p>
-          <p className={clsx("mb-12")}>{dictionary.about.paragraph4}</p>
-          {lang === "en" ? (
-            <p className={clsx("mb-4")}>{dictionary.about.paragraph5}</p>
-          ) : null}
-          <p className={clsx("mb-4")}>{dictionary.about.paragraph6}</p>
-          <p className={clsx("mb-12 text-2xl")}>[&emsp;]</p>
-          <p>{dictionary.about.quote}</p>
+          <p className={clsx("mb-4 w-full")}>{dictionary.about.paragraph1}</p>
+          <p className={clsx("mb-4 w-full")}>[&emsp;]</p>
+          <VideoPlayer
+            className={clsx("mb-12 w-full")}
+            heightRatio={IRRATIONAL_CUBE_VIDEO.heightRatio}
+            playbackId={IRRATIONAL_CUBE_VIDEO.playbackId}
+            widthRatio={IRRATIONAL_CUBE_VIDEO.widthRatio}
+          />
+          <p className={clsx("mb-4 w-full")}>{dictionary.about.paragraph2}</p>
+          <p className={clsx("mb-4 w-full")}>{dictionary.about.paragraph3}</p>
+          <p className={clsx("mb-4 w-full")}>{dictionary.about.paragraph4}</p>
+          <p className={clsx("mb-4 w-full")}>{dictionary.about.paragraph5}</p>
+          <p className={clsx("mb-4 w-full")}>[&emsp;]</p>
+          <VideoPlayer
+            className={clsx("mb-12 w-full")}
+            heightRatio={CRYING_GIRL_VIDEO.heightRatio}
+            playbackId={CRYING_GIRL_VIDEO.playbackId}
+            widthRatio={CRYING_GIRL_VIDEO.widthRatio}
+          />
         </div>
       </div>
     </>
