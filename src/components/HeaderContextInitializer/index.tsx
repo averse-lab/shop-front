5e1ce443b45db64861f9937ccd79fd36b3d125d1
alex-mngt <mongeot.alex@gmail.com>
2024-05-16 14:04:49 +0200

    feat: update contexts logics

--- src/components/HeaderContextInitializer/index.tsx
+++ src/components/HeaderContextInitializer/index.tsx
@@ -1,42 +1,80 @@
 "use client";
 
-import { FC, useContext, useEffect } from "react";
+import { FC, use, useEffect } from "react";
 
 import {
+  HEADER_CONTEXT_INIT,
   HeaderContext,
   HeaderContextValue,
 } from "@contexts/HeaderContext/HeaderContext";
 
-type IProps = {} & Partial<
-  Pick<HeaderContextValue, "hideLogo" | "whiteIcons" | "whiteBackground">
+type IProps = Pick<
+  HeaderContextValue,
+  | "logoVisible"
+  | "headerBgColor"
+  | "menuBtnColor"
+  | "menuBtnIcnColor"
+  | "menuBgColor"
+  | "cartBtnColor"
+  | "cartBtnIcnColor"
 >;
 
 export const HeaderContextInitializer: FC<IProps> = (props) => {
-  const { hideLogo, whiteIcons, whiteBackground } = props;
-  const { setHideLogo, setWhiteIcons, setWhiteBackground } =
-    useContext(HeaderContext) || {};
+  const {
+    logoVisible,
+    headerBgColor,
+    menuBtnColor,
+    menuBtnIcnColor,
+    menuBgColor,
+    cartBtnColor,
+    cartBtnIcnColor,
+  } = props;
+  const {
+    setLogoVisible,
+    setHeaderBgColor,
+    setMenuBgColor,
+    setMenuBtnColor,
+    setMenuBtnIcnColor,
+    setCartBtnColor,
+    setCartBtnIcnColor,
+  } = use(HeaderContext);
 
   useEffect(() => {
     if (
-      setHideLogo === undefined ||
-      setWhiteIcons === undefined ||
-      setWhiteBackground === undefined
+      !setLogoVisible ||
+      !setHeaderBgColor ||
+      !setMenuBgColor ||
+      !setMenuBtnColor ||
+      !setMenuBtnIcnColor ||
+      !setCartBtnColor ||
+      !setCartBtnIcnColor
     ) {
       return;
     }
 
-    setHideLogo(hideLogo || false);
-    setWhiteIcons(whiteIcons || false);
-    setWhiteBackground(whiteBackground || false);
+    setLogoVisible(logoVisible ?? HEADER_CONTEXT_INIT.logoVisible);
+    setHeaderBgColor(headerBgColor ?? HEADER_CONTEXT_INIT.headerBgColor);
+    setMenuBtnColor(menuBtnColor ?? HEADER_CONTEXT_INIT.menuBtnColor);
+    setMenuBtnIcnColor(menuBtnIcnColor ?? HEADER_CONTEXT_INIT.menuBtnIcnColor);
+    setCartBtnColor(cartBtnColor ?? HEADER_CONTEXT_INIT.cartBtnColor);
+    setCartBtnIcnColor(cartBtnIcnColor ?? HEADER_CONTEXT_INIT.cartBtnIcnColor);
+    setMenuBgColor(menuBgColor ?? HEADER_CONTEXT_INIT.menuBgColor);
   }, [
-    hideLogo,
-    setHideLogo,
-
-    setWhiteBackground,
-    setWhiteIcons,
-
-    whiteBackground,
-    whiteIcons,
+    cartBtnColor,
+    cartBtnIcnColor,
+    headerBgColor,
+    logoVisible,
+    menuBgColor,
+    menuBtnColor,
+    menuBtnIcnColor,
+    setCartBtnColor,
+    setCartBtnIcnColor,
+    setHeaderBgColor,
+    setLogoVisible,
+    setMenuBgColor,
+    setMenuBtnColor,
+    setMenuBtnIcnColor,
   ]);
+
   return <></>;
 };
