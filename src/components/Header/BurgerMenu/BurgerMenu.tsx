eab61095f3d6443431862d077e5b342db7da2e64
alex-mngt <mongeot.alex@gmail.com>
2024-05-17 17:29:12 +0200

    chore: convert scss to tailwind for Menu and Cart

--- src/components/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -52,7 +52,11 @@ export const BurgerMenu: FC<IProps> = (props) => {
       <Backdrop activate={open} />
       <Button
         aria-label={openBurgerMenuAriaLabel}
-        className={clsx(className, s["burger-menu__burger"])}
+        className={clsx(
+          className,
+          "[&:hover+div]:-translate-x-[calc(100%-8px)]",
+          open && "[&:hover+div]:!translate-x-2",
+        )}
         iconColor={menuBtnIcnColor}
         onClick={openMenu}
         size='icon'
@@ -62,12 +66,11 @@ export const BurgerMenu: FC<IProps> = (props) => {
       </Button>
       <div
         className={clsx(
-          s["burger-menu__menu"],
-          open && s["burger-menu__menu--open"],
-          "fixed left-0 top-0 z-20 md:left-2 md:top-2",
+          "fixed left-0 top-0 z-20 md:top-2",
           "h-dvh w-screen p-6 md:h-auto md:w-auto",
           "flex flex-col",
-          "uppercase text-primary-foreground backdrop-blur md:rounded md:border md:shadow-lg",
+          "-translate-x-full uppercase text-primary-foreground backdrop-blur transition-all md:rounded md:border md:shadow-lg",
+          open && "translate-x-2",
           menuBgColor === "white"
             ? "bg-secondary/20 md:border-border/20"
             : "bg-primary/85 md:border-border/10",
--- src/components/Header/Cart/Cart.tsx
+++ src/components/Header/Cart/Cart.tsx
@@ -68,18 +68,21 @@ export const Cart: FC<IProps> = (props) => {
       <Backdrop activate={cartOpen} />
       <CartButton
         ariaLabel={openCartAriaLabel}
-        className={clsx(className, s["cart__trigger"])}
+        className={clsx(
+          className,
+          "[&:hover+div]:translate-x-[calc(100%-8px)]",
+          cartOpen && "[&:hover+div]:!-translate-x-2",
+        )}
         onClick={openCart}
         quantity={cart?.totalQuantity}
       />
       <div
         className={clsx(
-          s["cart__modal"],
-          cartOpen && s["cart__modal--open"],
-          "fixed right-0 top-0 z-20 md:right-2 md:top-2",
+          "fixed right-0 top-0 z-20 md:top-2",
           "h-dvh w-screen px-6 pb-4 pt-6 md:h-auto md:max-h-[70vh] md:w-[450px] md:p-6",
           "flex flex-col justify-between gap-6",
-          "bg-secondary/20 backdrop-blur md:rounded md:border md:border-border/20 md:shadow-lg",
+          "translate-x-full bg-secondary/20 backdrop-blur transition-all md:rounded md:border md:border-border/20 md:shadow-lg",
+          cartOpen && "-translate-x-2",
         )}
         ref={cartRef}
       >
