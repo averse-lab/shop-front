ce36273f1884975a17250ccc007c371c93c0d98f
Alex Mongeot <mongeot.alex@protonmail.com>
2024-05-21 12:55:11 +0200

    fix: menu position on hover (#40)
    * fix: menu position on hover
    
    * chore: update dependencies
    
    * feat: update Backdrop transition duration to 500ms
    
    ---------
    
    Co-authored-by: Thibault Walterspieler <twalterspieler@pm.me>

--- src/components/Header/Cart/Cart.tsx
+++ src/components/Header/Cart/Cart.tsx
@@ -70,8 +70,8 @@ export const Cart: FC<IProps> = (props) => {
         ariaLabel={openCartAriaLabel}
         className={clsx(
           className,
-          "[&:hover+div]:translate-x-[calc(100%-8px)]",
-          cartOpen && "[&:hover+div]:!-translate-x-2",
+          "lg:[&:hover+div]:translate-x-[calc(100%-8px)]",
+          cartOpen && "lg:[&:hover+div]:!-translate-x-2",
         )}
         onClick={openCart}
         quantity={cart?.totalQuantity}
@@ -81,8 +81,8 @@ export const Cart: FC<IProps> = (props) => {
           "fixed right-0 top-0 z-20 md:top-2",
           "h-dvh w-screen px-6 pb-4 pt-6 md:h-auto md:max-h-[70vh] md:w-[450px] md:p-6",
           "flex flex-col justify-between gap-6",
-          "translate-x-full bg-secondary/20 backdrop-blur transition-all md:rounded md:border md:border-border/20 md:shadow-lg",
-          cartOpen && "-translate-x-2",
+          "bg-secondary/20 backdrop-blur transition-all md:rounded md:border md:border-border/20 md:shadow-lg",
+          cartOpen ? "-translate-x-2" : "translate-x-full",
         )}
         ref={cartRef}
       >
