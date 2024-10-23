3929aae6d8a04a6e124bf6526ce19273c1a1f84c
Thibault Walterspieler <twalterspieler@pm.me>
2024-10-23 15:47:14 +0200

    feat: disable intl

--- src/lib/i18n/config.ts
+++ src/lib/i18n/config.ts
@@ -1,6 +1,6 @@
 import { Config, Locale } from "./types";
 
-export const I18N_LOCALES: Locale[] = ["en", "fr"];
+export const I18N_LOCALES: Locale[] = ["en"];
 
 export const I18N_CONFIG: Config = {
   defaultLocale: "en",
