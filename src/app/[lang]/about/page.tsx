a351bf6ad7889bb93032dfcf61e06f05dfb07bef
Thibault Walterspieler <twalterspieler@pm.me>
2024-11-17 15:37:03 +0100

    fix: remove useless player

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -8,6 +8,7 @@ import { VideoPlayer } from "@components/VideoPlayer/VideoPlayer";
 
 import { Locale } from "@lib/i18n/types";
 import { getDictionary } from "@lib/i18n/utils";
+import { getMuxPlaceholder } from "@lib/mux/utils";
 import { PAGES } from "@lib/routing/constants";
 import { generateAlternates } from "@lib/utils";
 
@@ -70,12 +71,23 @@ const AboutPage: FC<IProps> = async (props) => {
 
   const dictionary = await getDictionary(lang);
 
+  const aboutVideoPlaceholder = await getMuxPlaceholder({
+    playbackId: ABOUT_VIDEO.playbackId,
+    width: 64,
+  });
+  const irrationalCubeVideoPlaceholder = await getMuxPlaceholder({
+    playbackId: IRRATIONAL_CUBE_VIDEO.playbackId,
+    width: 64,
+  });
+  const cryingGirlVideoPlaceholder = await getMuxPlaceholder({
+    playbackId: CRYING_GIRL_VIDEO.playbackId,
+    width: 64,
+  });
+
   return (
     <>
       <HeaderContextInitializer hideLogo whiteIcons />
-      <div
-        className={clsx("min-h-screen px-6 py-4", "flex flex-col", "bg-black")}
-      >
+      <div className={clsx("min-h-screen px-6 py-4", "flex flex-col", "bg-black")}>
         <div
           className={clsx(
             "m-auto mt-[72px] md:mt-[96px]",
@@ -86,42 +98,24 @@ const AboutPage: FC<IProps> = async (props) => {
           )}
         >
           <VideoPlayer
-            className={clsx("mb-12", "max-w-[90%] md:max-w-44")}
-            heightRatio={ABOUT_VIDEO.heightRatio}
+            className={clsx("mb-12 w-full", `aspect-[4/3]`)}
+            minResolution='720p'
+            placeholder={aboutVideoPlaceholder}
             playbackId={ABOUT_VIDEO.playbackId}
-            widthRatio={ABOUT_VIDEO.widthRatio}
           />
           <p className={clsx("mb-4 w-full")}>{dictionary.about.paragraph1}</p>
           <p className={clsx("mb-4 w-full")}>[&emsp;]</p>
           <VideoPlayer
-            className={clsx("mb-12 w-full")}
-            heightRatio={IRRATIONAL_CUBE_VIDEO.heightRatio}
-            playbackId={IRRATIONAL_CUBE_VIDEO.playbackId}
-            widthRatio={IRRATIONAL_CUBE_VIDEO.widthRatio}
+            className={clsx("mb-12 w-full", `aspect-[347/450]`)}
+            minResolution='720p'
+            placeholder={cryingGirlVideoPlaceholder}
+            playbackId={CRYING_GIRL_VIDEO.playbackId}
           />
           <p className={clsx("mb-4 w-full")}>{dictionary.about.paragraph2}</p>
           <p className={clsx("mb-4 w-full")}>{dictionary.about.paragraph3}</p>
           <p className={clsx("mb-4 w-full")}>{dictionary.about.paragraph4}</p>
           <p className={clsx("mb-4 w-full")}>{dictionary.about.paragraph5}</p>
           <p className={clsx("mb-4 w-full")}>[&emsp;]</p>
-          <VideoPlayer
-            className={clsx("mb-12 w-full")}
-            heightRatio={CRYING_GIRL_VIDEO.heightRatio}
-            playbackId={CRYING_GIRL_VIDEO.playbackId}
-            widthRatio={CRYING_GIRL_VIDEO.widthRatio}
-          />
-          <p
-            className={clsx(
-              "mb-4 flex h-[500px] w-full flex-col gap-10 text-[#454545]",
-            )}
-            style={{
-              writingMode: "vertical-rl",
-              textOrientation: "mixed",
-            }}
-          >
-            <span>{dictionary.about.paragraph7}</span>
-            <span>{dictionary.about.quote}</span>
-          </p>
         </div>
       </div>
     </>
